ddb3bd23495d44751caf427856a5faeaceefb4ab
ChapAl94 <albert.chap0707@gmail.com>
2022-02-16 01:05:37 -0500

    first commit

---
+++ website/auth.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth', __name__)
+
+# @auth.route('/symptoms')
+# def symptoms():
+#     return render_template("symptoms.html")
+
+# @auth.route('/medication')
+# def medication():
+#     return render_template("medication.html")
---
+++ website/views.py
@@ -0,0 +1,21 @@
+#stores routes not relating to authenication whre user can naviagte too
+
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
+@views.route('/symptoms')
+def symptoms():
+    return render_template("symptoms.html")
+
+@views.route('/medication')
+def medication():
+    return render_template("medication.html")
+
+
+
+
