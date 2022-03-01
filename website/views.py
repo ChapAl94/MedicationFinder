813a3fc8c6adb78bc14720cd257638f4720b5797
ChapAl94 <albert.chap0707@gmail.com>
2022-03-01 03:56:59 -0500

    Integration of Microservice

--- website/views.py
+++ website/views.py
@@ -1,19 +1,28 @@
 #stores routes not relating to authenication whre user can naviagte too
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def home():
     return render_template("home.html")
 
-@views.route('/symptoms')
+@views.route('/symptoms' , methods = ['GET', 'POST'])
 def symptoms():
+    if request.method == 'POST':
+        print(request.form.get('symptomCheckBox'))
+        return 'Done'
     return render_template("symptoms.html")
 
-@views.route('/medication')
+@views.route('/medication', methods=['GET','POST'])
 def medication():
+    if request.method == 'POST':
+        medTitle = request.form['med']
+        with open('medicationPrice.txt','w') as f:
+            f.write(medTitle)
+            
     return render_template("medication.html")
 
 @views.route('/guide')
