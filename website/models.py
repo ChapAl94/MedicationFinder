1695153a63ae269e61d3a2f0764d507c049c255f
Chapal94 <chapal@oregonstate.edu>
2022-03-18 14:58:10 -0400

    add class medication for database

--- website/models.py
+++ website/models.py
@@ -1,12 +1,14 @@
 from . import db 
 
+
+
 class medication(db.Model):
     id = db.Column("id", db.Integer, primary_key=True)
     medicationName = db.Column(db.String(100))
     symptom = db.Column(db.String(100))
     location = db.Column(db.String(100))
 
-    def __init__(self, medicationName, symptom, location):
-        self.medicationName = medicationName
-        self.symptom = symptom
-        self.location = location
+    # def __init__(self, medicationName, symptom, location):
+    #     self.medicationName = medicationName
+    #     self.symptom = symptom
+    #     self.location = location
